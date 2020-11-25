7da7a7984373e650ab0805f1581dff39b6156599
aliihsan123 <ihsan30092002@gmail.com>
2020-11-25 15:52:11 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//event pada saat link di klik
+$('.page-scroll').on('click', function(event){
+
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan = $(tujuan);
+
+    //pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo');
+
+    event.preventDefault();
+    
+});
+
+
+
+
+// paralax
+// about
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+
+
+    // Jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+
+    // Portfolio
+    if( wScroll > $('.portfolio').offset().top - 250 ){
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i+1));
+        });
+    }
+
+});
+
+
+
+
+
+
+
+
+
